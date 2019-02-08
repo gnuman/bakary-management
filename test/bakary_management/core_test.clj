f956d683f9510852699ddc10ed3cd2e076e8c3b9
anish patil <anish.developer@gmail.com>
2019-02-08 23:09:13 +1100

    Unit tests

--- test/bakary_management/core_test.clj
+++ test/bakary_management/core_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [bakary-management.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+
+(deftest can-product-breakdown-equally-succes
+  (testing "Testing if prouduct can be breakdown successfully"
+    (is (= (can-product-breakdown-equally 10 [5 2]) [{:pack-type 5, :num-of-packs 2}] )))
+  )
+
+(deftest can-product-breakdown-equally-failure
+  (testing "Testing if prouduct can not be breakdown "
+    (is (= (can-product-breakdown-equally 11 [5 2]) {:err "Can't devide equally"} )))
+  )
+
+
+(deftest test-product-in-inventory
+  (testing "Testing product is in inventory"
+    (is (= (check-product-in-inventory "CF" inventory) "CF" )))
+  )
